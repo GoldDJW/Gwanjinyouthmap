9c5336c7d78ea552a292970b5aee26bad4e8ab40
GoldDJW <chanmei03@gmail.com>
2026-07-04 15:33:41 +0900

    Add files via upload

---
+++ config.js
@@ -0,0 +1,3 @@
+/* 이 파일은 package_builder.html 또는 build_site.py가 원본 엑셀의 지도제작용_최종 시트로부터 생성합니다. */
+window.GWANGJIN_DATA_META = { count: 0, version: '', generatedAt: '' };
+window.GWANGJIN_INSTITUTIONS = [];
