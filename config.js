25903705eed5510e74d882490e3c745bfef18802
GoldDJW <chanmei03@gmail.com>
2026-07-04 15:41:37 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,6 @@
 /* 이 파일은 package_builder.html 또는 build_site.py가 원본 엑셀의 지도제작용_최종 시트로부터 생성합니다. */
 window.GWANGJIN_DATA_META = { count: 0, version: '', generatedAt: '' };
 window.GWANGJIN_INSTITUTIONS = [];
+window.GWANGJIN_MAP_CONFIG = {
+  KAKAO_JAVASCRIPT_KEY: "1f7e33df0f406de47e4333ff3570fa24"
+};
